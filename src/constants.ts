8f27c5e1fc486cf8e1835ef86c4cded7726fa962
Valeriy Panov <Panov_Valeriy@mail.ru>
2025-04-13 22:01:23 +0300

    Для отображения иконке на PROD теперь не нужно править index.html

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const SERVER = 'http://localhost:8000/'
-// export const SERVER = '/'
+// export const SERVER = 'http://localhost:8000/'
+export const SERVER = '/'
 
 export const API_SERVER = `${SERVER}api/`
 
